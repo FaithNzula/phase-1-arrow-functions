2b92d2235360591fd63ec253205d3c8bdb74bf8f
Faith Nzula Matei <faithnzula1230@gmail.com>
2022-08-22 15:21:31 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function ( num1,num2) {
+    num1 = 2000
+    num2 = 100
+    const answer = num1 / num2;
+    return answer;
+}
+let square = (a, b) => {
+     return a * a;
+};
+ 
+let add = (a, b) => {
+    return a + b;
+ }
